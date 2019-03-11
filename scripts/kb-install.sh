ea717fc711b801e60de614e065aab5f02eff735c
Slav Donchev <slav.donchev@gmail.com>
2019-03-11 10:45:32 +0200

    lock versions of installed elastic components

--- scripts/kb-install.sh
+++ scripts/kb-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Install Kibana
 
-sudo apt-get update && sudo apt-get install -y kibana
+sudo apt-get update && sudo apt-get install -y kibana=6.6.1
 
 # enable Kibana service
 sudo /bin/systemctl daemon-reload
